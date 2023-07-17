02e8202bde3c8e65d523f0ebf5405f457286a356
Gaurav Rao <gaurav18115@gmail.com>
2023-07-17 15:17:22 +0530

    Logger and config fix

--- logger.py
+++ logger.py
@@ -2,8 +2,28 @@ import logging
 import logging.handlers
 
 from logging.handlers import TimedRotatingFileHandler
-from pythonjsonlogger import jsonlogger
 
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 def setup_logger(filename):
     logger = logging.getLogger()
@@ -12,8 +32,8 @@ def setup_logger(filename):
     # Create console handler with a different formatter (not JSON)
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
-    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
-    ch.setFormatter(console_formatter)
+    color_formatter = CustomFormatter()
+    ch.setFormatter(color_formatter)
 
     if not filename:
         filename = 'logs/neo4jdb.log'
@@ -26,7 +46,7 @@ def setup_logger(filename):
     # jfh = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(message)s')
     # jfh.setFormatter(file_formatter)
 
-    fh.setFormatter(console_formatter)
+    fh.setFormatter(color_formatter)
 
     # Add the handlers to the logger
     logger.addHandler(ch)
